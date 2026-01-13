dce1c4fbf5d3b4c3bf8de448c5cd6575d9f6b338
Vaibhav <xyzx010101@gmail.com>
2026-01-13 16:20:17 +0530

    create utils files

--- src/index.js
+++ src/index.js
@@ -1,45 +1,18 @@
-// require("dotenv").config();
-import dotenv from "dotenv"//it is work with extra configures with pkg json dev scripts
+import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/index.js";
 const app = express();
 
 dotenv.config({
-    path:'./env'
+  path: "./env",
 });
 
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("ERROR :", error);
-//       throw error
-//     });
-//     app.listen(process.env.PORT,() => {
-//         console.log(`App is listening on port ${process.env.PORT}`)
-//     })
-//   } catch (error) {
-//     console.error("ERROR :", error);
-//     throw error;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
