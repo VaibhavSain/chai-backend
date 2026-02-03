0ab6d240ddb411976ac33fd584e79b0684a1d3c2
Vaibhav <xyzx010101@gmail.com>
2026-02-03 17:02:45 +0530

    added controller and routes

--- src/app.js
+++ src/app.js
@@ -27,5 +27,12 @@ app.use(
 app.use(express.static("public")); //any name by public
 
 app.use(cookieParser());
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+//http://localhost:8000/api/v1/users/register
 
 export { app };
