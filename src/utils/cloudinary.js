84f35d784c5c4597f8337ff630417c73318138d5
Vaibhav <xyzx010101@gmail.com>
2026-04-06 16:35:06 +0530

    updated and fixed the routes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,12 +5,9 @@ cloudinary.config({
 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 
-
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
-});
-
-
+});  
 
 const uploadOnCloudinary = async (localFilePath) => {
     
